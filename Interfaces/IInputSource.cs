ea95161baf7106b9641d1e37ffa354a1fdbdead3
ahineya0 <notheyarethesame@gmail.com>
2025-12-14 20:18:36 +0700

    Added class InputController and interface IInputSource

---
+++ Interfaces/IInputSource.cs
@@ -0,0 +1,11 @@
+﻿using elephantocracy.Enums;
+
+namespace InputForm
+{
+    public interface IInputSource
+    {
+        Direction? MoveDirection { get; }
+        bool FirePressed { get; }
+        void Reset();
+    }
+}
---
+++ Models/InputController.cs
@@ -0,0 +1,55 @@
+﻿using elephantocracy.Enums;
+using InputForm;
+
+public class InputController : IInputSource
+{
+    private readonly HashSet<Keys> pressedKeys = new();
+
+    private readonly Keys moveUpKey;
+    private readonly Keys moveDownKey;
+    private readonly Keys moveLeftKey;
+    private readonly Keys moveRightKey;
+    private readonly Keys fireKey;
+
+    private bool firePressed;
+
+    public InputController(Keys up, Keys down, Keys left, Keys right, Keys fire)
+    {
+        if (new HashSet<Keys> { up, down, left, right, fire }.Count != 5)
+            throw new ArgumentException("Клавиши управления должны быть уникальными");
+
+        moveUpKey = up;
+        moveDownKey = down;
+        moveLeftKey = left;
+        moveRightKey = right;
+        fireKey = fire;
+    }
+
+    public Direction? MoveDirection
+    {
+        get
+        {
+            if (pressedKeys.Contains(moveUpKey)) return Direction.Up;
+            if (pressedKeys.Contains(moveDownKey)) return Direction.Down;
+            if (pressedKeys.Contains(moveLeftKey)) return Direction.Left;
+            if (pressedKeys.Contains(moveRightKey)) return Direction.Right;
+            return null;
+        }
+    }
+
+    public bool FirePressed => firePressed;
+
+    public void OnKeyDown(Keys key)
+    {
+        pressedKeys.Add(key);
+        if (key == fireKey) firePressed = true;
+    }
+
+    public void OnKeyUp(Keys key)
+    {
+        pressedKeys.Remove(key);
+        if (key == fireKey) firePressed = false;
+    }
+
+    public void Reset() => firePressed = false;
+}
