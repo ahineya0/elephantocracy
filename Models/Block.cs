a4ee9b85b6f4c858ce32703e506191988aa700e5
Zaili324 <zaili324ru@gmail.com>
2025-12-14 18:01:35 +0700

    Update Block.cs
    Added logic for destructible blocks: blocks can take damage and be destroyed only if they are destructible

--- Models/Block.cs
+++ Models/Block.cs
@@ -2,32 +2,29 @@
 {
     public class Block
     {
-        public bool IsWalkable { get; private set; }      
-        public bool IsShootThrough { get; private set; }  
-        public int Health { get; private set; }           
-        public bool IsDestructible { get; private set; }  
+        public bool IsWalkable { get; private set; }
+        public bool IsShootThrough { get; private set; }
+        public int Health { get; private set; }
+        public bool IsDestructible { get; private set; } 
+
 
         public Block(bool isWalkable, bool isShootThrough, int health = 0, bool isDestructible = false)
         {
             IsWalkable = isWalkable;
             IsShootThrough = isShootThrough;
             Health = health;
-            IsDestructible = isDestructible;  
+            IsDestructible = isDestructible;
         }
-
-
         public void TakeDamage(int damage)
         {
-            if (Health > 0)
+            if (IsDestructible && Health > 0)
             {
-                Health -= damage;
+                Health -= damage;  
             }
         }
-
-  
         public bool IsDestroyed()
         {
-            return Health <= 0;
+            return (IsDestructible && Health <= 0);
         }
     }
 }
