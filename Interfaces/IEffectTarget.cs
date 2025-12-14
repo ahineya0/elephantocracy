fd1598f82374ef578ecbf2e4d46ca96404a83664
Zaili324 <zaili324ru@gmail.com>
2025-12-14 21:15:21 +0700

    Added Effect class
    Added freeze, speed boost, and life addition effects.

---
+++ Interfaces/IEffectTarget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using elephantocracy.Models;
+using elephantocracy.Enums;
+
+namespace elephantocracy.Interfaces
+{
+    public interface IEffectTarget
+    {
+        void ApplyFreeze();
+        void ApplySpeedBoost();
+        void AddLife();
+    }
+}
---
+++ Models/Effect.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using elephantocracy.Interfaces;
+
+namespace elephantocracy.Models
+{
+    public enum Effect
+    {
+        Freeze,
+        SpeedBoost,
+        Life
+    }
+
+    public class EffectModel
+    {
+        public Effect EffectType { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public float Duration { get; private set; }
+        public float ElapsedTime { get; private set; }
+        publci Effect(Effect effectType, int x, int y, float duration)
+        {
+            EffectType = effectType;
+            X = x;
+            Y = y;
+            Duration = duration;
+            ElapsedTime = 0;
+        }
+
+        public void Update(float deltaTime)
+        {
+            ElapsedTime += deltaTime;
+            if (ElapsedTime > Duration)
+        }
+
+        public void AppleTo(IEffectTarget target)
+        {
+            switch (EffectType)
+            {
+                case EffectType.Freeze:
+                    target.ApplyFreeze();  
+                    break;
+                case EffectType.SpeedBoost:
+                    target.ApplySpeedBoost();  
+                    break;
+                case EffectType.Life:
+                    target.AddLife();  
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
