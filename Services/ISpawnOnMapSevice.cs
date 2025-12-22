4f6190a4c3450b1874247214c1bf7e68133bd2f4
ahineya0 <notheyarethesame@gmail.com>
2025-12-22 13:04:55 +0700

    Added SpawnOnMapService

---
+++ Services/ISpawnOnMapSevice.cs
@@ -0,0 +1,12 @@
+﻿using elephantocracy.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elephantocracy.Services
+{
+    public interface ISpawnOnMapService
+    {
+        (int X, int Y) GetPlayerSpawn(Map map);
+    }
+}
---
+++ Services/SpawnOnMapService.cs
@@ -0,0 +1,39 @@
+﻿using elephantocracy.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elephantocracy.Services
+{
+    public class SpawnOnMapService : ISpawnOnMapService
+    {
+        private const int SpawnHealthMarker = 67;
+
+        public (int X, int Y) GetPlayerSpawn(Map map)
+        {
+            for (int x = 0; x < map.Width; x++)
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    Block block = map.GetBlock(x, y);
+                    if (block != null &&
+                        block.IsWalkable &&
+                        block.Health == SpawnHealthMarker)
+                    {
+                        return (x, y);
+                    }
+                }
+            }
+
+            for (int x = 0; x < map.Width; x++)
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    if (map.IsWalkable(x, y))
+                        return (x, y);
+                }
+            }
+            return (0, 0);
+        }
+    }
+}
